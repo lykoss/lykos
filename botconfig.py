ea14343ec2ba6fad043eb8d9e8feb5455c694d7a
Jimmy Cao <jcao219@gmail.com>
2011-08-04 22:15:37 -0500

    latest changes to botconfig.py

--- botconfig.py
+++ botconfig.py
@@ -7,4 +7,13 @@ OWNERS = ("unaffiliated/wolfbot_admin1",)  # the comma is required at the end if
 ADMINS = ("unaffiliated/wolfbot_admin2", "unaffiliated/wolfbot_admin3")
 CMD_CHAR = "!"
 CHANGING_HOST_QUIT_MESSAGE = "Changing host"
-DEBUG_MODE = False
+JOIN_AFTER_CLOAKED = True # Set to false if the bot does not have a cloak
+DISABLE_DEBUG_MODE = False  # Entirely disable debug mode
+
+# Argument --debug means start in debug mode
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--debug', action='store_true')
+
+args = parser.parse_args()
+DEBUG_MODE = args.debug if not DISABLE_DEBUG_MODE else False
