21872f39bce1a29665b9efd2d9146caad8719fd4
Eitan Adler <lists@eitanadler.com>
2013-05-12 11:51:44 -0400

    fix duplicate 'on on'

--- modules/wolfgame.py
+++ modules/wolfgame.py
@@ -2712,8 +2712,11 @@ def coin(cli, nick, chan, rest):
     cli.msg(chan, "\2{0}\2 tosses a coin into the air...".format(nick))
     var.LOGGER.logMessage("{0} tosses a coin into the air...".format(nick))
     coin = random.choice(["heads", "tails"])
-    if random.randrange(0, 20) == 0:
-        coin = "on its side"
+    specialty = random.randrange(0,20)
+    if specialty == 0:
+        coin = "its side"
+    if specialty == 1:
+        coin = botconfig.NICK
     cmsg = "The coin lands on \2{0}\2.".format(coin)
     cli.msg(chan, cmsg)
     var.LOGGER.logMessage(cmsg)
