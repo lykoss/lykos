f3c682861110d37a16e39ca0e0bf71618299fa10
nyuszika7h <nyuszika7h@gmail.com>
2014-07-15 21:03:31 +0200

    Improve !pstats output

--- settings/wolfgame.py
+++ settings/wolfgame.py
@@ -332,7 +332,7 @@ def get_player_totals(acc):
                     role_totals.append("\u0002{0}\u0002: {1}".format(role, *row))
             c.execute("SELECT SUM(totalgames) from rolestats WHERE player=? COLLATE NOCASE", (acc,))
             row = c.fetchone()
-            return "\u0002{0}\u0002's totals | {1} (total: {2})".format(player[0], ", ".join(role_totals), *row)
+            return "\u0002{0}\u0002's totals | \u0002{1}\u0002 games | {2}".format(player[0], *row ", ".join(role_totals))
         else:
             return "\u0002{0}\u0002 has not played any games.".format(acc)
             
