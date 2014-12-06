5cc9715eb14337fb4b5001c4a6debb66e479a398
Vgr E.Barry <vgr255@live.ca>
2014-12-06 14:59:48 -0500

    Move argument parsing out of botconfig.

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,3 +1,4 @@
+import argparse
 import botconfig
 from settings import wolfgame as var
 
@@ -13,3 +14,19 @@ for setting, value in botconfig.__dict__.items():
 
     # If we got that far, it's valid
     setattr(var, setting, value)
+
+# Handle launch parameters
+
+# Argument --debug means start in debug mode
+#          --verbose means to print a lot of stuff (when not in debug mode)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--debug', action='store_true')
+parser.add_argument('--sabotage', action='store_true')
+parser.add_argument('--verbose', action='store_true')
+
+args = parser.parse_args()
+botconfig.DEBUG_MODE = args.debug if not botconfig.DISABLE_DEBUG_MODE else False
+botconfig.VERBOSE_MODE = args.verbose
+
+botconfig.DEFAULT_MODULE = "sabotage" if args.sabotage else "wolfgame"
