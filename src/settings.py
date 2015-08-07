lykoss/lykos

commit 57930976263b3a3146b9fe3ce838b3f2064a0df1

Improve var.break_long_message
Concatenating strings repeatedly slows down the program and eats up
memory. It's not too bad for the number of players we have, but if you
get 10k players that makes a significant difference

src/settings.py
@@ -418,20 +418,20 @@ def role_order():
 
 
 def break_long_message(phrases, joinstr = " "):
-    message = ""
+    message = []
     count = 0
     for phrase in phrases:
         # IRC max is 512, but freenode splits around 380ish, make 300 to have plenty of wiggle room
         if count + len(joinstr) + len(phrase) > 300:
-            message += "\n" + phrase
-            count = len(phrase)
-        elif message == "":
-            message = phrase
+            message.append("\n" + phrase)
             count = len(phrase)
         else:
-            message += joinstr + phrase
-            count += len(joinstr) + len(phrase)
-    return message
+            if message:
+                count = len(phrase)
+            else:
+                count += len(joinstr) + len(phrase)
+            message.append(phrase)
+    return joinstr.join(message)
 
 class InvalidModeException(Exception): pass
 def game_mode(name, minp, maxp, likelihood = 0, conceal_roles = False):

commit 1b50a37b6935af2870d5ff36ad73ea55dec4b362

Fix break_long_message, broken in 57930976263b3a3146b9fe3ce838b3f2064a0df1

src/settings.py
@@ -426,7 +426,7 @@ def break_long_message(phrases, joinstr = " "):
             message.append("\n" + phrase)
             count = len(phrase)
         else:
-            if message:
+            if not message:
                 count = len(phrase)
             else:
                 count += len(joinstr) + len(phrase)