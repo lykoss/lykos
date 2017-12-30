688d086315cfd03beebf76b2211a2892311cfddf
jacob1 <jfu614@gmail.com>
2017-12-29 19:43:59 -0500

    fix piper

--- src/roles/piper.py
+++ src/roles/piper.py
@@ -42,7 +42,7 @@ def charm(var, wrapper, message):
     orig2 = target2
 
     evt1 = Event("targeted_command", {"target": target1.nick, "misdirection": True, "exchange": True})
-    evt1.dispatch(wrapper.client, var, "charm", wrapper.source.nick, target1.nick, frozenset({"detrimental"}))
+    evt1.dispatch(var, "charm", wrapper.source.nick, target1.nick, frozenset({"detrimental"}))
     if evt1.prevent_default:
         return
     target1 = users._get(evt1.data["target"]) # FIXME: need to make targeted_command use users
