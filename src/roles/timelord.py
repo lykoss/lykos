2def9ad445cdd7f0ae821e8de373bd70de9b2003
Ammon Smith <ammon.i.smith@gmail.com>
2018-04-24 17:52:26 -0700

    Fix imports to newest skeleton.

--- src/roles/timelord.py
+++ src/roles/timelord.py
@@ -2,13 +2,14 @@ import re
 import random
 import itertools
 import math
-import time
 from collections import defaultdict
 
 import botconfig
 from src.utilities import *
 from src import channels, users, debuglog, errlog, plog
+from src.functions import get_players, get_all_players, get_main_role, get_reveal_role, get_target
 from src.decorators import command, event_listener
+from src.containers import UserList, UserSet, UserDict, DefaultUserDict
 from src.messages import messages
 from src.events import Event
 
