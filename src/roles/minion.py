e2e0d7d7434db660c1ca21b6c19302753bd9cb3a
Ammon Smith <ammon.i.smith@gmail.com>
2018-05-26 04:28:36 -0700

    Split minion.

---
+++ src/roles/minion.py
@@ -0,0 +1,50 @@
+import re
+import random
+import itertools
+import math
+from collections import defaultdict
+
+import botconfig
+from src.utilities import *
+from src import channels, users, debuglog, errlog, plog
+from src.functions import get_players, get_all_players, get_main_role, get_reveal_role, get_target
+from src.decorators import command, event_listener
+from src.containers import UserList, UserSet, UserDict, DefaultUserDict
+from src.messages import messages
+from src.events import Event
+
+def wolf_list(var):
+    wolves = [wolf.nick for wolf in get_all_players(var.WOLF_ROLES)]
+    random.shuffle(wolves)
+    return "Wolves: {}".format(", ".join(wolves))
+
+# PM players who have this role with instructions
+# Set priority=2 if this is a main role, and priority=5 if this is a secondary role
+# (secondary roles are like gunner, assassin, etc. which by default stack on top of main roles)
+@event_listener("transition_night_end", priority=2)
+def on_transition_night_end(evt, var):
+    if var.FIRST_NIGHT or var.ALWAYS_PM_ROLE:
+        for minion in get_all_players(("minion",)):
+            if minion.prefers_simple():
+                minion.send(messages["minion_simple"])
+            else:
+                minion.send(messages["minion_notify"])
+            minion.send(wolf_list(var))
+
+@event_listener("exchange_roles")
+def on_exchange(evt, var, actor, target, actor_role, target_role):
+    if actor_role == "minion":
+        evt.data["target_messages"].append(wolf_list(var))
+    elif target_role == "minion":
+        evt.data["actor_messages"].append(wolf_list(var))
+
+@event_listener("myrole")
+def on_myrole(evt, var, user):
+    if user in get_all_players(("minion",)):
+        wolves = []
+        for wolfrole in var.WOLF_ROLES:
+            for player in var.ORIGINAL_ROLES[wolfrole]:
+                wolves.append(player.nick)
+        user.send(messages["original_wolves"].format(", ".join(wolves)))
+
+# vim: set sw=4 expandtab:
