3f59e6fd8348a44bc17b9522465cdc42c9426227
Ammon Smith <ammon.i.smith@gmail.com>
2018-05-26 20:39:31 -0700

    Remove wild child from wolves when notifying.

--- src/roles/wildchild.py
+++ src/roles/wildchild.py
@@ -85,6 +85,7 @@ def on_del_player(evt, var, user, mainrole, allroles, death_triggers):
                 wcroles = var.WOLF_ROLES | {"traitor"}
 
             wolves = get_players(wcroles)
+            wolves.remove(child)
             if wolves:
                 for wolf in wolves:
                     wolf.queue_message(messages["wild_child_as_wolf"].format(child))
