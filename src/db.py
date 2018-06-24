9d4bc1e1df49b61d714b199d1033b17308de0176
simplexish <38588344+electricalabrocoma@users.noreply.github.com>
2018-06-24 16:58:07 +0000

    change variable name

--- src/db.py
+++ src/db.py
@@ -359,14 +359,14 @@ def get_player_totals(acc, hostmask):
                         JOIN person pe
                           ON pl.person = pe.id
                         WHERE pe.id = ?""", (peid,))
-    winrate = c.fetchone()[0]
+    won_games = c.fetchone()[0]
     order = role_order()
     name = _get_display_name(peid)
     #ordered role stats
     totals = ["\u0002{0}\u0002: {1}".format(r, tmp[r]) for r in order if r in tmp]
     #lover or any other special stats
     totals += ["\u0002{0}\u0002: {1}".format(r, t) for r, t in tmp.items() if r not in order]
-    return "\u0002{0}\u0002's totals | \u0002{1}\u0002 games | Winrate: \u0002{2:.0%}\u0002 | {3}".format(name, total_games, winrate / total_games, break_long_message(totals, ", "))
+    return "\u0002{0}\u0002's totals | \u0002{1}\u0002 games | Winrate: \u0002{2:.0%}\u0002 | {3}".format(name, total_games, won_games / total_games, break_long_message(totals, ", "))
 
 def get_game_stats(mode, size):
     conn = _conn()
