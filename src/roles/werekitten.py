378a98774e19cc6794beebbc1aa3fe2276e3a738
Vgr E. Barry <vgr255@live.ca>
2018-10-04 15:30:14 -0400

    Split werekitten

---
+++ src/roles/werekitten.py
@@ -0,0 +1,23 @@
+import re
+import random
+import itertools
+import math
+from collections import defaultdict
+
+from src.utilities import *
+from src import channels, users, debuglog, errlog, plog
+from src.functions import get_players, get_all_players, get_main_role, get_reveal_role, get_target
+from src.decorators import command, event_listener
+from src.containers import UserList, UserSet, UserDict, DefaultUserDict
+from src.messages import messages
+from src.events import Event
+
+@event_listener("gun_shoot")
+def on_gun_shoot(evt, var, user, target):
+    if get_main_role(target) == "werekitten":
+        evt.data["hit"] = False
+
+@event_listener("get_role_metadata")
+def on_get_role_metadata(evt, var, kind):
+    if kind == "role_categories":
+        evt.data["werekitten"] = {"Wolf", "Wolfchat", "Wolfteam", "Innocent", "Killer", "Nocturnal"}
