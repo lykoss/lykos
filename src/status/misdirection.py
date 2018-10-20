26a8f82069f44df1e9be502dd5604214f56b8062
Vgr E. Barry <vgr255@live.ca>
2018-10-20 15:12:44 -0400

    Make sure to apply misdirection sequentially
    Previously I explicitly did it sequentially, but misdirection as actor applies first, and the original target may be different by the time you reach misdirection as target, which is why it's important to do it sequentially. In that case, it may result in the same target as initially, or two targets over in either direction. Either way, this is the proper way to do it.

--- src/status/misdirection.py
+++ src/status/misdirection.py
@@ -9,6 +9,16 @@ __all__ = ["add_misdirection", "try_misdirection"]
 MISDIRECTED = UserSet() # type: Set[users.User]
 LUCKY = UserSet() # type: Set[users.User]
 
+def _get_target(target):
+    """Internal helper for try_misdirection. Return one target overt."""
+    pl = get_players()
+    index = pl.index(target)
+    if random.randint(0, 1):
+        index -= 2
+    if index == len(pl) - 1: # last item
+        index = -1
+    return pl[index+1]
+
 def add_misdirection(var, user, *, as_actor=False, as_target=False):
     if as_actor:
         MISDIRECTED.add(user)
@@ -17,17 +27,11 @@ def add_misdirection(var, user, *, as_actor=False, as_target=False):
 
 def try_misdirection(var, actor, target):
     """Check if misdirection can apply. Return the target."""
-    # If the actor is misdirected but the target is lucky, they cancel each other out
-    # If we did it sequencially, there'd be a chance of targeting the second neighbor
-    if actor is not target and (actor in MISDIRECTED) ^ (target in LUCKY):
-        pl = get_players()
-        index = pl.index(target)
-        if random.randint(0, 1):
-            index -= 2
-        if index == len(pl) - 1: # last item
-            index = -1
-        target = pl[index+1]
-
+    if actor is not target:
+        if actor in MISDIRECTED:
+            target = _get_target(target)
+        if target in LUCKY:
+            target = _get_target(target)
     return target
 
 @event_listener("del_player")
