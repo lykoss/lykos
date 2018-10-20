c4dd8c45dd9a1bcfbe75889144bb6aa131ecb4bb
Vgr E. Barry <vgr255@live.ca>
2018-10-20 15:16:16 -0400

    Rename internal misdirection variables

--- src/status/misdirection.py
+++ src/status/misdirection.py
@@ -6,8 +6,8 @@ from src.functions import get_players
 
 __all__ = ["add_misdirection", "try_misdirection"]
 
-MISDIRECTED = UserSet() # type: Set[users.User]
-LUCKY = UserSet() # type: Set[users.User]
+AS_ACTOR = UserSet() # type: Set[users.User]
+AS_TARGET = UserSet() # type: Set[users.User]
 
 def _get_target(target):
     """Internal helper for try_misdirection. Return one target overt."""
@@ -21,44 +21,44 @@ def _get_target(target):
 
 def add_misdirection(var, user, *, as_actor=False, as_target=False):
     if as_actor:
-        MISDIRECTED.add(user)
+        AS_ACTOR.add(user)
     if as_target:
-        LUCKY.add(user)
+        AS_TARGET.add(user)
 
 def try_misdirection(var, actor, target):
     """Check if misdirection can apply. Return the target."""
     if actor is not target:
-        if actor in MISDIRECTED:
+        if actor in AS_ACTOR:
             target = _get_target(target)
-        if target in LUCKY:
+        if target in AS_TARGET:
             target = _get_target(target)
     return target
 
 @event_listener("del_player")
 def on_del_player(evt, var, player, allroles, death_triggers):
-    MISDIRECTED.discard(player)
-    LUCKY.discard(player)
+    AS_ACTOR.discard(player)
+    AS_TARGET.discard(player)
 
 @event_listener("revealroles")
 def on_revealroles(evt, var, wrapper):
-    if MISDIRECTED or LUCKY:
-        misdirected = MISDIRECTED | LUCKY
+    if AS_ACTOR or AS_TARGET:
+        misdirected = AS_ACTOR | AS_TARGET
         out = []
         for user in misdirected:
             as_what = []
-            if user in MISDIRECTED:
+            if user in AS_ACTOR:
                 as_what.append("actor")
-            if user in LUCKY:
+            if user in AS_TARGET:
                 as_what.append("target")
             out.append("{0} (as {1})".format(user, " and ".join(as_what)))
         evt.data["output"].append("\u0002misdirected\u0002: {0}".format(", ".join(out)))
 
 @event_listener("transition_day_begin")
 def on_transition_day_begin(evt, var):
-    MISDIRECTED.clear()
-    LUCKY.clear()
+    AS_ACTOR.clear()
+    AS_TARGET.clear()
 
 @event_listener("reset")
 def on_reset(evt, var):
-    MISDIRECTED.clear()
-    LUCKY.clear()
+    AS_ACTOR.clear()
+    AS_TARGET.clear()
