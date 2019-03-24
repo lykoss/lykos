00f9de0dee96eb263f097f93fb47f5ca5a8bcae3
jacob1 <jfu614@gmail.com>
2019-03-23 21:06:45 -0400

    replace broken gif link

--- src/wolfgame.py
+++ src/wolfgame.py
@@ -2423,7 +2423,7 @@ def transition_day(gameid=0):
             "https://i.imgur.com/mUcM09n.gifv",
             "https://i.imgur.com/b8HAvjL.gifv",
             "https://i.imgur.com/PIIfL15.gifv",
-            "https://i.imgur.com/eJiMG5z.gifv"]
+            "https://i.imgur.com/nly0Cmm.gifv"]
             ))
     channels.Main.send("\n".join(to_send))
 
