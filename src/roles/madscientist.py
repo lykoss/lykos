f99e51774e8d4b60ea33abd074b7c448707ed77a
jacob1 <jfu614@gmail.com>
2019-11-12 22:52:22 -0500

    Fix mad scientist transition_night_end message

--- src/roles/madscientist.py
+++ src/roles/madscientist.py
@@ -91,7 +91,7 @@ def on_transition_night_end(evt, var):
         pl = get_players()
         target1, target2 = _get_targets(var, pl, ms)
 
-        ms.send(messages["mad_scientist_notify"], messages["mad_scientist_myrole_targets"].format(target1, target2), sep=" ")
+        ms.send(messages["mad_scientist_notify"].format(target1, target2))
 
 @event_listener("myrole")
 def on_myrole(evt, var, user):
