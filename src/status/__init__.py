f6873e4262f8e7983578886595cef631a2f8aadf
Ryan Schmidt <skizzerz@skizzerz.net>
2019-12-04 08:37:09 -0700

    Fix IDE compatibility for cats and statuses
    By dynamically munging __all__, certain IDEs (such as PyCharm) do not
    pick up on the changes we've made to it and instead treat our code as if
    it has errors due to symbols not being defined. Make things more
    explicit instead of being clever, even though it does mean more code in
    the files and a bit of extra boilerplate down the line if adding
    additional cats or statuses. The tradeoff is worthwhile.

--- src/status/__init__.py
+++ src/status/__init__.py
@@ -1,27 +1,28 @@
 """Core game status are exposed here."""
 
-import os.path
-import glob
-import importlib
+# For better IDE auto completion, don't try to replace this with a loop
+__all__ = [
+    "add_absent", "get_absent", "try_absent",
+    "add_disease", "remove_disease", "wolves_diseased",
+    "add_dying", "is_dying",
+    "add_exchange", "try_exchange",
+    "add_force_vote", "add_force_abstain", "get_forced_votes", "get_forced_abstains",
+    "add_lycanthropy", "add_lycanthropy_scope", "remove_lycanthropy",
+    "add_lynch_immunity", "try_lynch_immunity",
+    "add_misdirection", "try_misdirection",
+    "add_protection", "remove_all_protections", "try_protection",
+    "add_silent", "is_silent",
+    "add_vote_weight", "get_vote_weight", "remove_vote_weight"
+]
 
-__all__ = []
-
-path = os.path.dirname(os.path.abspath(__file__))
-search = os.path.join(path, "*.py")
-
-for f in glob.iglob(search):
-    f = os.path.basename(f)
-    n, _ = os.path.splitext(f)
-    if f.startswith("_"):
-        continue
-    mod = importlib.import_module("." + n, package="src.status")
-
-    all = mod.__all__
-    if set(__all__) & set(all):
-        raise TypeError("Error: duplicate names {0}".format(set(__all__) & set(all)))
-    __all__.extend(all)
-
-    for name in all:
-        globals()[name] = getattr(mod, name)
-
-# vim: set sw=4 expandtab:
+from src.status.absent import *
+from src.status.disease import *
+from src.status.dying import *
+from src.status.exchange import *
+from src.status.forcevote import *
+from src.status.lycanthropy import *
+from src.status.lynchimmune import *
+from src.status.misdirection import *
+from src.status.protection import *
+from src.status.silence import *
+from src.status.voteweight import *
