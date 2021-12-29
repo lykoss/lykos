f06219d5481a92c5c5e4759fed49afb839c40984
Ryan Schmidt <skizzerz@skizzerz.net>
2021-12-28 19:33:26 -0700

    Kaboom balance fix
    Wolf needed a lot of help in this mode, so let's make one of them
    blessed. At later counts, we ensure that the wolf oracle is *not* also
    the blessed wolf.
    
    If this swings too far in wolves' favor, next up is probably to make
    master of teleportation a village-aligned role instead of neutral. I
    might end up doing that anyway just so there aren't too many roles with
    the "must survive to win" condition in a mode with a lot of splash
    damage from MS and VG.

--- src/gamemodes/kaboom.py
+++ src/gamemodes/kaboom.py
@@ -1,5 +1,7 @@
+from src.events import EventListener
 from src.gamemodes import game_mode, GameMode
-from src.messages import messages
+from src.gamestate import GameState
+
 
 @game_mode("kaboom", minp=6, maxp=24, likelihood=0)
 class KaboomMode(GameMode):
@@ -7,7 +9,7 @@ class KaboomMode(GameMode):
     def __init__(self, arg=""):
         super().__init__(arg)
         self.ROLE_GUIDE = {
-            6: ["wolf", "crazed shaman", "mad scientist", "insomniac", "vengeful ghost"],
+            6: ["wolf", "crazed shaman", "mad scientist", "insomniac", "vengeful ghost", "blessed villager"],
             7: ["master of teleportation"],
             8: ["wolf"],
             10: ["oracle", "mad scientist"],
@@ -23,4 +25,19 @@ class KaboomMode(GameMode):
             22: ["mad scientist"],
             23: ["vengeful ghost"]
         }
+        self.SECONDARY_ROLES["blessed villager"] = {"wolf"}
         self.SECONDARY_ROLES["oracle"] = {"wolf"}
+        self.EVENTS = {
+            "role_attribution_end": EventListener(self.on_role_attribution_end)
+        }
+
+    def on_role_attribution_end(self, evt, var: GameState, main_roles, roles):
+        # ensure the blessed wolf and the wolf oracle aren't the same wolf
+        if not roles.get("oracle", None):
+            return
+        blessed = list(roles["blessed villager"])[0]
+        oracle = list(roles["oracle"])[0]
+        if blessed is oracle:
+            evt.data["actions"].append(("remove", oracle, "oracle"))
+            other = list(roles["wolf"] - {oracle})[0]
+            evt.data["actions"].append(("add", other, "oracle"))
